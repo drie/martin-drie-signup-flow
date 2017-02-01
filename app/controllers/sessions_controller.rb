07db41f4a6c0e057755e7ae2484fe8d8a085e914
drie martin <martin@drie.co>
2017-02-01 17:50:33 +0100

    Adding logic to write to #signups slack channel everytime there is a new signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,6 @@
+require 'net/http'
+require 'json'
+
 class SessionsController < ApplicationController
   def new
   end
@@ -6,6 +9,19 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
 
     if user.valid?
+      msg = user.email + "," + user.username
+
+      uri = URI('http://master.drie-flask-services.app.push.drieapp.co/send-slack-msg-to-drie')
+      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+      req.body = {channel: '#signups', msg: msg}.to_json
+
+      # Write email to slack channel #signups
+      res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+          http.request(req)
+      end
+
+      puts res.body
+
       session[:user_id] = user.id
       redirect_to request.env['omniauth.origin']
     end
