9387f7f2e1832d67cf292113927a6f6c62336e7d
drie martin <martin@drie.co>
2017-02-02 11:02:08 +0100

    Putting send_slack_msg in it's own method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,17 +10,7 @@ class SessionsController < ApplicationController
 
     if user.valid?
       msg = user.email + "," + user.username
-
-      uri = URI('http://master.drie-flask-services.app.push.drieapp.co/send-slack-msg-to-drie')
-      req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-      req.body = {channel: '#signups', msg: msg}.to_json
-
-      # Write email to slack channel #signups
-      res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-          http.request(req)
-      end
-
-      puts res.body
+      send_slack_msg("@martin", msg)
 
       session[:user_id] = user.id
       redirect_to request.env['omniauth.origin']
@@ -29,8 +19,21 @@ class SessionsController < ApplicationController
 
   def destroy
     #reset_session
-    #session[:user_id] = nil
-    session.delete(:user_id)
+    session[:user_id] = nil
     redirect_to request.referer
+  end
+
+
+  def send_slack_msg(channel, msg)
+    uri = URI('http://master.drie-flask-services.app.push.drieapp.co/send-slack-msg-to-drie')
+    req = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+    req.body = {channel: channel, msg: msg}.to_json
+
+    # Write email to slack channel #signups
+    res = Net::HTTP.start(uri.hostname, uri.port) do |http|
+      http.request(req)
+    end
+
+    puts res.body
   end
 end
